07590992da04a297522d32c7b3f28419cdcf6c4e
Alen Scaria Antony <63664995+alenscaria@users.noreply.github.com>
2023-09-07 22:20:55 +0530

    updated app.py

--- app.py
+++ app.py
@@ -1,6 +1,12 @@
 from flask import Flask,render_template, request
 app=Flask(__name__) 
 
+
+
+@app.route('/') 
+def home():
+    return render_template('home.html')
+
 @app.route('/feedback') 
 def feedback():
     return render_template('feedback.html')
